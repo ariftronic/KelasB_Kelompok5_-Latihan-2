8905c88823786f242a0291f6e983f82730956219
hilang-arah <bintangmlazuardi@student.uns.ac.id>
2026-06-02 09:15:31 +0700

    fitur-subclass-koleksi
    oke

--- uts-perpustakaan-oop/models/buku.py
+++ uts-perpustakaan-oop/models/buku.py
@@ -1,1 +1,14 @@
+from core.koleksi import Koleksi
 
+# [OOP] Inheritance: Buku mewarisi sifat dari Koleksi
+# [SOLID] Liskov Substitution Principle (LSP): Objek Buku bisa menggantikan Koleksi tanpa error
+class Buku(Koleksi):
+    def __init__(self, kode, judul, tahun, pengarang, penerbit):
+        super().__init__(kode, judul, tahun)
+        self.jenis = "Buku"
+        self.pengarang = pengarang
+        self.penerbit = penerbit
+
+    # [OOP] Polymorphism: Implementasi spesifik untuk Buku
+    def tampilkan_info(self):
+        return f"Jenis\t\t: {self.jenis}\nKode Koleksi\t: {self.kode}\nJudul\t\t: {self.judul}\nThn Terbit\t: {self.tahun}\nPengarang\t: {self.pengarang}\nPenerbit\t: {self.penerbit}\n"
--- uts-perpustakaan-oop/models/jurnal.py
+++ uts-perpustakaan-oop/models/jurnal.py
@@ -1,1 +1,12 @@
+from core.koleksi import Koleksi
 
+class Jurnal(Koleksi):
+    def __init__(self, kode, judul, tahun, penerbit, bidang_studi, impact_factor):
+        super().__init__(kode, judul, tahun)
+        self.jenis = "Jurnal"
+        self.penerbit = penerbit
+        self.bidang_studi = bidang_studi
+        self.impact_factor = impact_factor
+
+    def tampilkan_info(self):
+        return f"Jenis\t\t: {self.jenis}\nKode Koleksi\t: {self.kode}\nJudul\t\t: {self.judul}\nThn Terbit\t: {self.tahun}\nPenerbit\t: {self.penerbit}\nImpact Factor\t: {self.impact_factor}\nBidang Studi\t: {self.bidang_studi}\n"
